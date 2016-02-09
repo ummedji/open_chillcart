559690b0badb2502c6aa91390743cd237e82b47e
sureshb1209 <suresh1209@gmail.com>
2016-02-09 21:16:34 +0000

    setup fixes

--- app/Config/core.php
+++ app/Config/core.php
@@ -121,7 +121,7 @@ Configure::write('Config.language', 'eng');
  * Turn off all caching application-wide.
  *
  */
-	Configure::write('Cache.disable', true);
+	//Configure::write('Cache.disable', true);
 
 /**
  * Enable cache checking.
@@ -262,22 +262,22 @@ $prefix = 'myapp_';
  * Configure the cache used for general framework caching.  Path information,
  * object listings, and translation cache files are stored with this configuration.
  */
-//Cache::config('_cake_core_', array(
-//	'engine' => $engine,
-//	'prefix' => $prefix . 'cake_core_',
-//	'path' => CACHE . 'persistent' . DS,
-//	'serialize' => ($engine === 'File'),
-//	'duration' => $duration
-//));
+Cache::config('_cake_core_', array(
+	'engine' => $engine,
+	'prefix' => $prefix . 'cake_core_',
+	'path' => CACHE . 'persistent' . DS,
+	'serialize' => ($engine === 'File'),
+	'duration' => $duration
+));
 
 /**
  * Configure the cache for model and datasource caches.  This cache configuration
  * is used to store schema descriptions, and table listings in connections.
  */
-//Cache::config('_cake_model_', array(
-//	'engine' => $engine,
-//	'prefix' => $prefix . 'cake_model_',
-//	'path' => CACHE . 'models' . DS,
-//	'serialize' => ($engine === 'File'),
-//	'duration' => $duration
-//));
+Cache::config('_cake_model_', array(
+	'engine' => $engine,
+	'prefix' => $prefix . 'cake_model_',
+	'path' => CACHE . 'models' . DS,
+	'serialize' => ($engine === 'File'),
+	'duration' => $duration
+));
